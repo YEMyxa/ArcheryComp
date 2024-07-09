2f26ae82b94a938e0ea6249ec7613a40fad7fef9
Ivan Krasnov <ivan.k.05russia@gmail.com>
2024-07-09 18:05:45 +0300

    Edit Participations List

--- ArcheryComp/participations/urls.py
+++ ArcheryComp/participations/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from participations import views
 
 app_name = 'participations'
@@ -6,6 +6,10 @@ app_name = 'participations'
 urlpatterns = [
     path('', views.SportsmanListView.as_view(), name='sportsmans_list'),
     path('profile/<int:id>/', views.profile, name='profile'),
-    path('<int:id>/participations_list/', views.PersonalParticipationListView.as_view(), name='participations_list'),
+    path('<int:id>/', include([
+        path('participations_list/', views.PersonalParticipationListView.as_view(), name='participations_list'),
+        path('team_participations_list/', views.TeamParticipationListView.as_view(), name='team_participations_list'),
+        path('mixed_participations_list/', views.MixedParticipationListView.as_view(), name='mixed_participations_list'),
+    ])),
     path('<str:username>', views.UserParticipitionsView.as_view(), name='user_part')
 ]
--- ArcheryComp/participations/views.py
+++ ArcheryComp/participations/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
 from .models import Sportsman, PersonalParticipation, TeamParticipation, MixedParticipation
+from django.db.models import Q
 
 from django.views.generic import ListView
 
@@ -17,7 +18,7 @@ class SportsmanListView(ListView):
         sportsmans = self.get_queryset().order_by('last_name')
         sportsmans_html = '<h1>Список спортсменов</h1><ul>'
         for sportsman in sportsmans:
-            profile_url = reverse('participations:profile', kwargs={'id': sportsman.id})
+            profile_url = reverse('users:profile_view', kwargs={'username': sportsman.profile.user.username})
             sportsmans_html += f'<li><a href="{profile_url}">{sportsman.last_name} {sportsman.first_name}</a></li>'
         sportsmans_html += '</ul>'
         return HttpResponse(sportsmans_html)
@@ -26,34 +27,94 @@ class PersonalParticipationListView(ListView):
     model = PersonalParticipation
 
     def get(self, request, *args, **kwargs):
-        participations = self.get_queryset().order_by('competition')
+        competition_prev = 0
+        participations = PersonalParticipation.objects.filter(sportsman=self.kwargs['id'])
         participations_html = '<h2>Список личных участий</h2><ul>'
         for participation in participations:
             competition = participation.competition
             competition_url = reverse('competitions:competition_detail',
                                       kwargs={'comp_id': competition.comp_id})
-            participations_html += (f'<li><a href="{competition_url}">{participation.competition.title}</a></br>'
-                                    f'      {participation.program.name} {participation.place} '
+            if competition != competition_prev:
+                participations_html += f'<a href="{competition_url}">{participation.competition.title}</a></br>'
+            participations_html += (f'<li>    {participation.program.name} {participation.place} '
                                     f'{participation.place_qualification} {participation.sum_qualification}</li>')
+            competition_prev = competition
+        participations_html += '</ul>'
+        return HttpResponse(participations_html)
+
+class TeamParticipationListView(ListView):
+    model = TeamParticipation
+
+    def get(self, request, *args, **kwargs):
+        competition_prev = 0
+        participations = TeamParticipation.objects.filter(Q(sportsman_1=self.kwargs['id'])
+                                                          | Q(sportsman_2=self.kwargs['id'])
+                                                          | Q(sportsman_3=self.kwargs['id']))
+        participations_html = '<h2>Список командных участий</h2><ul>'
+        for participation in participations:
+            competition = participation.competition
+            competition_url = reverse('competitions:competition_detail',
+                                      kwargs={'comp_id': competition.comp_id})
+            team = [participation.sportsman_1, participation.sportsman_2, participation.sportsman_3]
+            team.remove(get_object_or_404(Sportsman, pk=self.kwargs['id']))
+            if competition != competition_prev:
+                participations_html += f'<a href="{competition_url}">{participation.competition.title}</a></br>'
+            participations_html += (f'<li>    {participation.program.name} {participation.place} '
+                                    f'{participation.place_qualification} {participation.sum_qualification}</br>'
+                                    f'в команде с ')
+            for i in range(2):
+                teammate_url = reverse('users:profile_view', kwargs={'username': team[i].profile.user.username})
+                participations_html += f'<a href="{teammate_url}">{team[i].last_name} {team[i].first_name} </a>'
+            participations_html += '</li>'
+            competition_prev = competition
+        participations_html += '</ul>'
+        return HttpResponse(participations_html)
+
+class MixedParticipationListView(ListView):
+    model = MixedParticipation
+
+    def get(self, request, *args, **kwargs):
+        competition_prev = 0
+        sportsman = get_object_or_404(Sportsman, pk=self.kwargs['id'])
+        if sportsman.sex == 'M':
+            participations = MixedParticipation.objects.filter(sportsman_M = self.kwargs['id'])
+        else:
+            participations = MixedParticipation.objects.filter(sportsman_F=self.kwargs['id'])
+        participations_html = '<h2>Список смешанных командных участий</h2><ul>'
+        for participation in participations:
+            competition = participation.competition
+            competition_url = reverse('competitions:competition_detail',
+                                      kwargs={'comp_id': competition.comp_id})
+            if competition != competition_prev:
+                participations_html += f'<a href="{competition_url}">{participation.competition.title}</a></br>'
+            participations_html += (f'<li>    {participation.program.name} {participation.place} '
+                                    f'{participation.place_qualification} {participation.sum_qualification}</br>'
+                                    f'в команде с ')
+            if sportsman.sex == 'M':
+                teammate = participation.sportsman_F
+            else:
+                teammate = participation.sportsman_M
+            teammate_url = reverse('users:profile_view', kwargs={'username': teammate.profile.user.username})
+            participations_html += f'<a href="{teammate_url}">{teammate.last_name} {teammate.first_name} </a>'
+            participations_html += '</li>'
+            competition_prev = competition
         participations_html += '</ul>'
         return HttpResponse(participations_html)
 
 def profile(request, id):
     participations_list_url = reverse('participations:participations_list', kwargs={'id': id})
-    html = (f'<h1>Профиль спортсмена {id}</h1>'
-            f'<a href="{participations_list_url}">Список личных уччастий</a>')
+    team_participations_list_url = reverse('participations:team_participations_list', kwargs={'id': id})
+    mixed_participations_list_url = reverse('participations:mixed_participations_list', kwargs={'id': id})
+    html = (f'<h1>Участия спортсмена {id}</h1>'
+            f'<a href="{participations_list_url}">Список личных участий</a></br>'
+            f'<a href="{team_participations_list_url}">Список командных участий</a></br>'
+            f'<a href="{mixed_participations_list_url}">Список смешанных командных участий</a>')
     return HttpResponse(html)
 
 from django.views.generic import CreateView
 from .forms import PersonalParticipationForm, TeamParticipationForm, MixedParticipationForm
 from django.shortcuts import render, redirect, get_object_or_404
 from competitions.models import Competition, Program
-
-def index(request):
-    html = f"<h1>Список спортсменов</h1>"
-    return HttpResponse(html)
-
-
 
 class ParticipationCreateView(CreateView):
     template_name = 'participations/add_participation.html'
